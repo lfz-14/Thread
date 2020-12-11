a0a8ce5f36cc40d460eff8eaf1552e924a975734
LFZ <2018110449@mail.scuec.edu.cn>
2020-12-11 16:08:26 +0800

    多进程

--- processing/basic.py
+++ processing/basic.py
@@ -1,8 +1,13 @@
 # import threading
+
 import time
 import multiprocessing
 
-
+# 主进程，子进程
+# 进程占用的资源较大，线程占用的资源少
+# 进程数不是越多越好，根据电脑的资源来分配
+# 写时拷贝  多线程应用更多
+# 现有进程，再有线程
 def test1():
     while True:
         print("1-----")
